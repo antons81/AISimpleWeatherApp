1d83fbaf5290656849663a836f17d86ec812d2cf
Anton Stremovskiy <perlik@gmail.com>
2026-04-11 13:55:12 +0200

    Initial Commit

---
+++ AISimpleWeatherApp/AISimpleWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AISimpleWeatherAppApp.swift
+//  AISimpleWeatherApp
+//
+//  Created by Anton Stremovskiy on 11.04.26.
+//
+
+import SwiftUI
+
+@main
+struct AISimpleWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
