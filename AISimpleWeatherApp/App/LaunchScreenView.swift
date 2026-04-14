7a1e7a6ecebed60ac6b97bae727bf0b09166c649
Anton Stremovskiy <perlik@gmail.com>
2026-04-14 16:10:51 +0200

    changed project name, added tests and splash screen

---
+++ AISimpleWeatherApp/App/LaunchScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchScreenView.swift
+//  WearlyWeather
+//
+//  Created by Anton Stremovskiy on 14.04.26.
+//
+
+import SwiftUI
+
+struct LaunchScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchScreenView()
+}
---
+++ AISimpleWeatherAppTests/AISimpleWeatherAppTests.swift
@@ -0,0 +1,18 @@
+//
+//  AISimpleWeatherAppTests.swift
+//  AISimpleWeatherAppTests
+//
+//  Created by Anton Stremovskiy on 14.04.26.
+//
+
+import Testing
+
+struct AISimpleWeatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
